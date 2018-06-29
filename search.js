6d2c8592e41d1ff2742f7779a7c9fc25280d6311
viperML <ayatsfer@gmail.com>
2018-06-29 11:28:00 +0100

    search query

--- search.js
+++ search.js
@@ -14,46 +14,35 @@ String.prototype.replaceChars = function(character, replacement){
  
 function search(query){
     switch(query.substr(0, 2)){
-        case "-a":
-            query = query.substr(3);
-            window.location = "http://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=" +
-                query.replaceChars(" ", "+");
-            break;
-
         case "-y":
             query = query.substr(3);
             window.location =
-                "https://www.youtube.com/results?search_query=" +
-                query.replaceChars(" ", "+");
+            "https://www.youtube.com/results?search_query=" +
+            query.replaceChars(" ", "+");
             break;
 
-        case "-w":
+	    case "-w":
+	        query = query.substr(3);
+	        window.location = 
+		    "http://www.wolframalpha.com/input/?i=" + 
+		    query.replaceChars("+", "%2B");
+		    break;
+            
+        case "-m":
+		    query=query.substr(3);
+		    window.location = 
+			"https://forum.mobilism.org/search.php?keywords=" + 
+            query.replaceChars(" ", "+") + "&fid[]=398&sr=topics&sf=titleonly";
+            break;
+            
+        case "-n":
             query = query.substr(3);
             window.location =
-                "https://en.wikipedia.org/w/index.php?search=" +
-                query.replaceChars(" ", "%20");
-            break;
+            "https://nyaa.si/?f=0&c=0_0&s=seeders&o=desc&q=" +
+            query.replaceChars(" ", "+");
+        break;
 
-	case "-m":
-	    query = query.substr(3);
-	    window.location = 
-		"http://www.wolframalpha.com/input/?i=" + 
-		query.replaceChars("+", "%2B");
-		break;
 
-	case "-h":
-		query=query.substr(3);
-		window.location = 
-			"http://alpha.wallhaven.cc/search?q=" + 
-			query.concat("&categories=111&purity=100&resolutions=1920x1080&sorting=relevance&order=desc");
-			break;
-
-//        case "-4":
- //           query = query.substr(3);
- //           window.location =
-  //              "http://boards.4chan.org/" +
- //               query.replaceChars(" ", "%20");
-//            break;
         default:
             window.location="https://www.google.com/#q=" +
                 query.replaceChars(" ", "+");
