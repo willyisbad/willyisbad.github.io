3e1f2afcd34329c936ffb68af1808d694ed7f9ee
viperML <ayatsfer@gmail.com>
2019-02-20 18:58:01 +0100

    new photos

--- img/convert.ps1
+++ img/convert.ps1
@@ -1,16 +1,22 @@
 $Source = Get-ChildItem ".\*.png"
-$DestinationA = ".\"
+$Destination = ".\"
 
 
 ForEach ($files in $Source) {
-    cwebp $files -q 85 -m 6 -o $($DestinationA + $files.BaseName + ".webp")
+    cwebp $files -q 85 -m 6 -o $($Destination + $files.BaseName + ".webp")
 }
 
 
 $Source2 = Get-ChildItem ".\*.jpg"
-$DestinationB = ".\"
 
 
 ForEach ($files2 in $Source2) {
-    cwebp $files2 -q 85 -m 6 -o $($DestinationB + $files2.BaseName + ".webp")
+    cwebp $files2 -q 85 -m 6 -o $($Destination + $files2.BaseName + ".webp")
+}
+
+$Source3 = Get-ChildItem ".\*.jpeg"
+
+
+ForEach ($files3 in $Source3) {
+    cwebp $files3 -q 85 -m 6 -o $($Destination3 + $files3.BaseName + ".webp")
 }
